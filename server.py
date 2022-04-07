531889e348de9b6aaccf7dd6b06725fa6420dddb
Andres Coronado <andres.coronadolzr@uanl.edu.mx>
2022-04-07 13:36:05 -0500

    python portafolio

---
+++ server.py
@@ -0,0 +1,54 @@
+from statistics import mode
+from flask import Flask, redirect, render_template, url_for, request
+app = Flask(__name__)
+
+@app.route('/index.html')
+def index():
+    return render_template('index.html')
+
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
+
+def write_to_file(data):
+    with open('database.txt',mode='a') as database:
+        email = data['email']
+        subject = data ['subject']
+        message = data['message']
+        file = database.write(f'\n {email},{subject}, {message}')
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        write_to_file(data)
+        print(data)
+        return redirect('/thankyou.html')
+    else:
+        return 'STH went wrong'
+
+# @app.route('/about.html')
+# def about():
+#     return render_template('about.html')
+
+# # @app.route('/components.html')
+# # def components():
+# #     return render_template('components.html')
+
+# @app.route('/contact.html')
+# def contact():
+#     return render_template('contact.html')
+
+
+# @app.route('/project.html')
+# def project():
+#     return render_template('project.html')
+
+# @app.route('/services.html')
+# def services():
+#     return render_template('services.html')
+
+# @app.route('/works.html')
+# def works():
+#     return render_template('works.html')
